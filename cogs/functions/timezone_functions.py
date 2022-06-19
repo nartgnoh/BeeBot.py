c6724aa803ca34568890e39a37879c7e96e93990
Hong-Hue Tran <hong.hue.tran@outlook.com>
2022-06-19 14:36:21 -0400

    fixing timezones

--- cogs/functions/timezone_functions.py
+++ cogs/functions/timezone_functions.py
@@ -3,4 +3,12 @@ from pytz import timezone
 
 
 def get_local_timezone():
+    return tz.tzlocal()
+
+
+def get_eastern_timezone():
     return timezone('US/Eastern')
+
+
+def get_pacific_timezone():
+    return timezone('US/Pacific')
